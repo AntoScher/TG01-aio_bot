dda8eeb4ad6cd4f550c30d15a56c388e5d1a7a2f
AntoScher <asc8er@gmail.com>
2025-01-14 15:47:12 +0300

    add API  you tube&weater

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
 from aiogram.handlers import MessageHandler
 from aiogram.types import Message
+import requests
 import re
 from pytube import YouTube
+
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
 
 API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+WEATHER_API_KEY = os.getenv('OPENWEATHERMAP_API_KEY')
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -32,10 +35,29 @@ async def start(message: Message):
 # Пример хендлера с использованием Command
 @dp.message(Command(commands=["help"]))
 async def help_command(message: Message):
-    await message.answer("Этот бот умеет выполнять команды:\n/start\n/help")
+    await message.answer("Этот бот умеет выполнять команды:\n/start\n/help\n/weather")
 
 
 # Прописываем хендлер для команды /weather
+@dp.message(Command(commands=["weather"]))
+async def weather_command(message: Message):
+    city = "minsk"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric&lang=ru"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["cod"] == 200:
+        temperature = data["main"]["temp"]
+        description = data["weather"][0]["description"]
+        await message.answer(f"Погода в {city.capitalize()}:\nТемпература: {temperature}°C\nОписание: {description}")
+    else:
+        await message.answer("Не удалось получить прогноз погоды. Пожалуйста, попробуйте позже.")
+
+
+
+
+
+
 @dp.message(lambda message: re.match(r'https?://(www\.)?youtube\.com/watch\?v=.+', message.text))
 async def youtube_info(message: Message):
     url = message.text
