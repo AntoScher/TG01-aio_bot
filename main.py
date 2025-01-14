7374bb1a050cbd91c6587864c760177ae6c2e110
AntoScher <asc8er@gmail.com>
2025-01-14 15:20:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.utils import executor
-from pytube import YouTube
+from aiogram.filters import CommandStart, Command
+from aiogram.handlers import MessageHandler
+from aiogram.types import Message
 import re
+from pytube import YouTube
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
@@ -19,17 +20,25 @@ logging.basicConfig(level=logging.INFO)
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
+dp = Dispatcher()
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет! Отправьте мне ссылку на YouTube-видео, и я пришлю информацию о нем.")
 
-@dp.message_handler(regexp=r'https?://(www\.)?youtube\.com/watch\?v=.+')
-async def youtube_info(message: types.Message):
+# Пример хендлера с использованием CommandStart
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Привет! Отправьте мне ссылку на YouTube-видео, и я пришлю информацию о нем.")
+
+
+# Пример хендлера с использованием Command
+@dp.message(Command(commands=["help"]))
+async def help_command(message: Message):
+    await message.answer("Этот бот умеет выполнять команды:\n/start\n/help")
+
+
+# Прописываем хендлер для команды /weather
+@dp.message(lambda message: re.match(r'https?://(www\.)?youtube\.com/watch\?v=.+', message.text))
+async def youtube_info(message: Message):
     url = message.text
-    # проверка ссылки на корректность
     if not re.match(r"^https://www\.youtube\.com/watch\?v=[a-zA-Z0-9_-]+$", url):
         await message.reply("Некорректная ссылка на YouTube. Пожалуйста, отправьте корректную ссылку.")
         return
@@ -44,5 +53,15 @@ async def youtube_info(message: types.Message):
         logging.error(f"Произошла ошибка при обработке ссылки: {e}")
         await message.reply(f"Произошла ошибка при обработке ссылки. Ошибка: {str(e)}")
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+
+async def main():
+    dp.message.register(start, CommandStart())
+    dp.message.register(help_command, Command(commands=["help"]))
+    dp.message.register(youtube_info,
+                        lambda message: re.match(r'https?://(www\.)?youtube\.com/watch\?v=.+', message.text))
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
